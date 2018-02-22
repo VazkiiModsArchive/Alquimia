3766205218bdfe565ed50a27bc1c694a9afdd672
Vasco Lavos <vazkii@hotmail.com>
2018-02-22 19:45:05 +0000

    Fix translation support.

--- src/main/java/vazkii/alquimia/client/lexicon/LexiconRegistry.java
+++ src/main/java/vazkii/alquimia/client/lexicon/LexiconRegistry.java
@@ -1,5 +1,6 @@
 package vazkii.alquimia.client.lexicon;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -31,66 +32,66 @@ import vazkii.alquimia.common.util.ItemStackUtil.StackWrapper;
 
 public class LexiconRegistry implements IResourceManagerReloadListener {
 
-	private static final String DEFAULT_LANG = "en_US";
+	private static final String DEFAULT_LANG = "en_us";
 	private static final ResourceLocation FALLBACK_CATEGORY = new ResourceLocation(LibMisc.MOD_ID, String.format("docs/%s/categories/fallback.json", DEFAULT_LANG));
 	private static final ResourceLocation FALLBACK_ENTRY = new ResourceLocation(LibMisc.MOD_ID, String.format("docs/%s/entries/fallback.json", DEFAULT_LANG));
 
 	public final List<ResourceLocation> rootFiles = new ArrayList();
-	
+
 	public final Map<ResourceLocation, LexiconCategory> categories = new HashMap();
 	public final Map<ResourceLocation, LexiconEntry> entries = new HashMap();
 	public final Map<String, Class<? extends LexiconPage>> pageTypes = new HashMap();
-	
+
 	public final Map<StackWrapper, Pair<LexiconEntry, Integer>> recipeMappings = new HashMap();
-	
+
 	private Gson gson;
 	private String currentLang;
-	
+
 	public static final LexiconRegistry INSTANCE = new LexiconRegistry();
-	
+
 	private LexiconRegistry() {
 		gson = new GsonBuilder()
 				.registerTypeHierarchyAdapter(LexiconPage.class, new LexiconPage.LexiconPageAdapter())
 				.create();
 	}
-	
+
 	public void init() {
 		addPageTypes();
-		
+
 		rootFiles.add(new ResourceLocation(LibMisc.MOD_ID, "docs/root.json"));
-		
+
 		IResourceManager manager = Minecraft.getMinecraft().getResourceManager();
 		if(manager instanceof IReloadableResourceManager)
 			((IReloadableResourceManager) manager).registerReloadListener(this);
 		else throw new RuntimeException("Minecraft's resource manager is not reloadable. Something went way wrong.");
 	}
-	
+
 	private void addPageTypes() {
 		pageTypes.put("text", PageText.class);
 		pageTypes.put("crafting", PageCrafting.class);
 		pageTypes.put("image", PageImage.class);
 	}
-	
+
 	public Pair<LexiconEntry, Integer> getEntryForStack(ItemStack stack) {
 		return recipeMappings.get(ItemStackUtil.wrapStack(stack));
 	}
-	
+
 	@Override
 	public void onResourceManagerReload(IResourceManager resourceManager) {
 		reloadLexiconRegistry();
 	}
-	
+
 	public void reloadLexiconRegistry() {
 		GuiLexicon.onReload();
 		categories.clear();
 		entries.clear();
 		recipeMappings.clear();
-		
+
 		currentLang = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode();
-		
+
 		List<LexiconRootObject> roots = new ArrayList();
 		rootFiles.forEach((res) -> roots.add(loadRootObject(res)));
-		
+
 		roots.forEach((root) -> {
 			root.categories.forEach(c -> {
 				ResourceLocation res = new ResourceLocation(root.namespace, c);
@@ -101,12 +102,12 @@ public class LexiconRegistry implements IResourceManagerReloadListener {
 				loadEntry(res, new ResourceLocation(res.getResourceDomain(), String.format("docs/%s/entries/%s.json", DEFAULT_LANG, res.getResourcePath())));
 			});
 		});
-		
+
 		entries.forEach((res, entry) -> entry.build(res));
 		categories.forEach((res, category) -> category.build(res));
 		ClientAdvancements.updateLockStatus();
 	}
-	
+
 	private LexiconRootObject loadRootObject(ResourceLocation res) {
 		InputStream stream = loadJson(res, null);
 		if(stream == null)
@@ -114,65 +115,68 @@ public class LexiconRegistry implements IResourceManagerReloadListener {
 
 		return gson.fromJson(new InputStreamReader(stream), LexiconRootObject.class);
 	}
-	
+
 	private void loadCategory(ResourceLocation key, ResourceLocation res) {
 		InputStream stream = loadLocalizedJson(res, FALLBACK_CATEGORY);
 		if(stream == null)
 			throw new IllegalArgumentException(res + " does not exist.");
-		
+
 		LexiconCategory category = gson.fromJson(new InputStreamReader(stream), LexiconCategory.class);
 		if(category == null)
 			throw new IllegalArgumentException(res + " does not exist.");
-		
+
 		categories.put(key, category);
 	}
-	
+
 	private void loadEntry(ResourceLocation key, ResourceLocation res) {
 		InputStream stream = loadLocalizedJson(res, FALLBACK_ENTRY);
 		if(stream == null)
 			throw new IllegalArgumentException(res + " does not exist.");
-		
+
 		LexiconEntry entry = gson.fromJson(new InputStreamReader(stream), LexiconEntry.class);
 		if(entry == null)
 			throw new IllegalArgumentException(res + " does not exist.");
-		
+
 		LexiconCategory category = entry.getCategory();
 		if(category != null)
 			category.addEntry(entry);
 		else new RuntimeException("Entry " + key + " does not have a valid category.").printStackTrace();
-		
+
 		entries.put(key, entry);	
 	}
-	
+
 	private InputStream loadLocalizedJson(ResourceLocation res, ResourceLocation fallback) {
 		ResourceLocation localized = new ResourceLocation(res.getResourceDomain(), res.getResourcePath().replaceAll(DEFAULT_LANG, currentLang));
-		
+
 		InputStream stream = loadJson(localized, null);
 		return stream == null ? loadJson(res, fallback) : stream;
 	}
-	
+
 	private InputStream loadJson(ResourceLocation resloc, ResourceLocation fallback) {
+		System.out.println("Loading " + resloc);
 		IResource res = null;
 		try {
 			res = Minecraft.getMinecraft().getResourceManager().getResource(resloc);
-		} catch (IOException e) { 
+		} catch (FileNotFoundException e) {
+			return null;
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		if(res == null && fallback != null) {
 			new RuntimeException("Alquimia failed to load " + resloc + ". Switching to fallback.").printStackTrace();
 			return loadJson(fallback, null);
 		}
-		
+
 		return res.getInputStream();
 	}
-	
+
 	static class LexiconRootObject {
-		
+
 		String namespace;
 		List<String> categories;
 		List<String> entries;
-		
+
 	}
-	
+
 }
