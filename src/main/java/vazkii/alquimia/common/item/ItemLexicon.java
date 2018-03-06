e26d531cef6f00e474621edf728b55f7264fb78b
Vasco Lavos <vazkii@hotmail.com>
2018-03-06 13:39:16 +0000

    Add a property override to the lexicon when open

--- src/main/java/vazkii/alquimia/common/item/ItemLexicon.java
+++ src/main/java/vazkii/alquimia/common/item/ItemLexicon.java
@@ -1,15 +1,25 @@
 package vazkii.alquimia.common.item;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import vazkii.alquimia.client.lexicon.gui.GuiLexicon;
 import vazkii.alquimia.common.Alquimia;
 import vazkii.alquimia.common.base.AlquimiaCreativeTab;
 import vazkii.alquimia.common.base.IAlquimiaItem;
@@ -22,6 +32,16 @@ public class ItemLexicon extends ItemMod implements IAlquimiaItem {
 		super("lexicon");
 		setMaxStackSize(1);
 		setCreativeTab(AlquimiaCreativeTab.INSTANCE);
+		
+		addPropertyOverride(new ResourceLocation("open"), new IItemPropertyGetter() {
+			
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, World worldIn, EntityLivingBase entityIn) {
+            	Minecraft mc = Minecraft.getMinecraft();
+            	return (entityIn == mc.player && mc.currentScreen instanceof GuiLexicon) ? 1 : 0;
+            }
+            
+        });
 	}
 	
 	@Override
